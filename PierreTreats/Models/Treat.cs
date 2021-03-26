094b5fe3dde8546e40d25481583d3493a68de595
Ashley Porter <porter.ashley13@gmail.com>
2021-03-26 09:54:25 -0700

    add models files to projects

---
+++ PierreTreats/Models/Treat.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace PierreTreats.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.JoinEntities = new HashSet<FlavorTreat>();
+    }
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Rating Rating { get; set; }
+    public string Ingredients { get; set; }
+    public string Instructions { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<FlavorTreat> JoinEntities { get; }
+  }
+
+  public enum Rating
+  {
+    One, // = 0
+    Two,
+    Three,
+    Four,
+    Five // = 4
+  }
+}
