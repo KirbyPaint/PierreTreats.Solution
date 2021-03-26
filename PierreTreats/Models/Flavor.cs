db238cf860f18703136b00fe52c472738e9cefdd
Ashley Porter <porter.ashley13@gmail.com>
2021-03-26 15:55:01 -0700

    add rating to Flavor table and on screen

--- PierreTreats/Models/Flavor.cs
+++ PierreTreats/Models/Flavor.cs
@@ -11,8 +11,17 @@ namespace PierreTreats.Models
 
     public int FlavorId { get; set; }
     public string Name { get; set; }
-    public int Rating { get; set; }
+    public FlavorRating Rating { get; set; }
     public virtual ApplicationUser User { get; set; }
     public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
+  }
+
+  public enum FlavorRating
+  {
+    One, // = 0
+    Two,
+    Three,
+    Four,
+    Five // = 4
   }
 }
